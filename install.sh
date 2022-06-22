e15cc4c4425e717ba91ba99081707eacae5571ec
nanashi <na@na.shi>
2022-06-22 11:07:04 +0800

    save work: add vim configs

--- install.sh
+++ install.sh
@@ -1,187 +1,190 @@
 #!/usr/bin/env bash
 
-function BACKUP_INIT(){
-    OLD_DOTFILES=~/"dotfile-$(date -u +"%Y%m%d%H%M%S")";echo "buck up old dotfiles may exist"
-    if ! uname -a | grep 'Linux' > /dev/null;then
-        if ! command -v diff > /dev/null;then
-            pacman -S diffutils
-        fi
-        echo '
-        %1 start "" mshta vbscript:CreateObject("Shell.Application").ShellExecute("cmd.exe","/c ""%~s0"" ::","","runas",1)(window.close)&&exit
-        ' > "$HOME"/mklink.bat
-    fi
-    # mkdir $OLD_DOTFILES
-    echo testing connection with google.com
-    USE_PROXY=$(ping -$(if uname -a | grep -P "^Linux" > /dev/null;then echo c;else echo n;fi) 2 google.com > /dev/null;echo $?)
-    NO_ZSH=$(command -v zsh > /dev/null;echo $?)
-    NO_VIM=$(command -v vim > /dev/null;echo $?)
-    NO_TMUX=$(command -v tmux > /dev/null;echo $?)
-    if [[ $USE_PROXY -ne 0 ]];then
-        echo -e "\033[31m Please specify your proxy or enter to use default proxy\033[0m \033[41;37m socks5://localhost:10808: \033[0m"
-        read -r PROXY
-        if [[ -z "$PROXY" ]];then
-            PROXY="socks5://localhost:10808";
-        fi
-	git config --global http.https://github.com.proxy $PROXY
-	git config --global https.https://github.com.proxy $PROXY
-    fi
+function check_folder_structure() {
+  test -d vim &&
+    test -d zsh &&
+    test -d tmux && return 0
+  echo "please run script inside root of repository" && exit 255
 }
 
-function SET_UP_APP(){
-    if uname -a | grep -P "^Linux" > /dev/null;then
-        # Linux
-        sudo apt update
-        if [[ $NO_ZSH -ne 0 ]];then
-            sudo apt install zsh
-            echo Try changing default shell to zsh... ||
-                grep -P "^$USER.*/bin/.*sh^" /etc/passwd | xargs -I line echo "\033[31m $(echo 'line' | sed -E "s%^(^USER.*)/bin/.*sh%\1$(command -v zsh)%") \033[0m"
-            read -r -p "Is it correct?(y/N)" ans
-            sudo cp /etc/passwd ~/passwd.bak && sudo sed -i -E "s%(^$USER.*)/bin/.*sh%\1$(command -v zsh)%" /etc/passwd &&
-            echo Success! || echo Failed, please check.
-        fi &&
-        if [[ $NO_VIM -ne 0 ]];then
-            sudo apt install vim
-        fi &&
-        if [[ $NO_TMUX -ne 0 ]];then
-            sudo apt install tmux
-        fi &&
-        return 0 || 
-        return 1
-    elif uname -a | grep -P "^MSYS" > /dev/null;then
-        # MSYS
-        if [[ $NO_ZSH -ne 0 ]];then
-            pacman -S zsh
-            echo -e '\033[31m Please edit config files(msys2/mingw32/mingw64/clang64/ucrt64.ini) in your MSYS2 install directory.\033[0m' 
-        fi &&
-        if [[ $NO_VIM -ne 0 ]];then
-            pacman -S vim
-        fi &&
-        if [[ $NO_TMUX -ne 0 ]];then
-            pacman -S tmux
-        fi &&
-        return 0 || 
-        return 1
-    elif uname -a | grep -P "^MINGW" > /dev/null;then
-        #Git bash
-        echo -e "\033[31m Sorry, Git bash doest support zsh! \033[0m"
-        return 1
-    else
-        echo -e "\033[31m Unknown Operating system! \033[0m"
-        return 1
+function set_up_variable() {
+  FMT_RED=$(printf '\033[31m')
+  FMT_GREEN=$(printf '\033[32m')
+  FMT_YELLOW=$(printf '\033[33m')
+  FMT_BLUE=$(printf '\033[34m')
+  FMT_BOLD=$(printf '\033[1m')
+  FMT_RESET=$(printf '\033[0m')
+  if command -v apt >>/dev/null; then
+    PAC_MAN="sudo apt install -y"
+    PAC_UPDATE="sudo apt update -y"
+  elif command -v yum >>/dev/null; then
+    PAC_MAN="sudo yum install -y"
+    PAC_UPDATE="sudo yum update -y"
+  elif command -v pacman >>/dev/null; then
+    echo "${FMT_RED}not support for arch${FMT_RESET}" && exit 255
+  else
+    echo "${FMT_RED}can't find any package manager${FMT_RESET}" && exit 255
+  fi
+  if ! nc -z -w 1 google.com 443 >/dev/null; then
+    echo -e "could not connect to google.com, please specify your proxy, or use ${FMT_BLUE}http://localhost:10809${FMT_RESET} by default:"
+    read -r PROXY
+    echo -e "proxy is: ${PROXY:=http://localhost:10809}"
+    if ! grep -P "^http://.*?:[[:digit:]]+$" <<<"$PROXY" >/dev/null 2>&1; then
+      echo -e 'proxy should match pattern "^http://.*?:[[:digit:]]+$"' && exit 255
     fi
+    if ! nc -z -w 1 \
+      $(sed -E "s/^http://(.*?):[[:digit:]]+$" <<<"$PROXY") \
+      $(sed -E "s/^http://.*?:([[:digit:]]+)$" <<<"$PROXY"); then
+      echo -e "${FMT_RED}could not connect to proxy!!${FMT_RESET}";
+      echo -e "continue anyway? [y/N]"
+      read -r opt;
+      case $opt in
+        y*|Y*)unset PROXY; ;;
+        n*|N*|"") exit 255; ;;
+      esac
+    fi
+  fi
 }
 
-function backup_if_exists() {
-    if [[ ! ( -d $OLD_DOTFILES ) ]];then
-        mkdir $OLD_DOTFILES
+function backup_and_link() {
+  if [ -h "$1" ]; then
+    echo -e "${FMT_YELLOW}original $1 points to $(readlink -f "$1")${FMT_RESET}";
+    if [ "$(readlink -f "$1")" == "$(readlink -f "$2")" ] ||
+        [ "$(readlink -f "$1")" -nt "$(readlink -f "$2")" ];then
+      return;
     fi
-    mv $1 $OLD_DOTFILES 2> /dev/null
+    rm -f "$1" || {
+      echo -e "${FMT_RED}failed remove $1${FMT_RESET}" && return 255
+    }
+    ln -s "$(readlink -f "$2")" "$1" || {
+      echo -e "${FMT_RED}failed create symbol link${FMT_RESET}" && return 255
+    }
+  elif [ "$1" -ot "$(readlink -f "$2")" ]; then
+    mv "$1" "$1.bak.$(date -u +"%Y%m%d%H%M%S")" || {
+      echo -e "${FMT_RED}failed create backup${FMT_RESET}" && return 255
+    }
+    ln -s "$(readlink -f "$2")" "$1" || {
+      echo -e "${FMT_RED}failed create symbol link${FMT_RESET}" && return 255
+    };
+  elif [ ! -e "$1" ]; then
+    ln -s "$(readlink -f "$2")" "$1" || {
+      echo -e "${FMT_RED}failed create symbol link${FMT_RESET}" && return 255
+    };
+  fi
 }
 
-# config zsh
-function CONFIG_ZSH(){
-    if [[ ! -d ~/.oh-my-zsh ]];then
-        if [[ $USE_PROXY -ne 0 ]];then
-            echo no | bash -c "$(curl -x $PROXY -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh | sed -E 's/github\.com/hub\.fastgit\.org/g')"
-        else
-            echo no | bash -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
-        fi
-    fi &&
-    if [[ ! -d ${ZSH_CUSTOM:-$HOME/.oh-my-zsh/custom}/themes/powerlevel10k ]];then
-        if [[ $USE_PROXY -ne 0 ]];then
-            git clone --depth=1 https://gitee.com/romkatv/powerlevel10k.git ${ZSH_CUSTOM:-$HOME/.oh-my-zsh/custom}/themes/powerlevel10k
-        else
-            git clone --depth=1 https://github.com/romkatv/powerlevel10k.git ${ZSH_CUSTOM:-$HOME/.oh-my-zsh/custom}/themes/powerlevel10k
-        fi
-        ln -s "$(pwd)/zsh/p10k.zsh" ~/.p10k.zsh
-        echo "Maybe you need to install fonts to get best experience"
-    fi &&
-    if [[ ! -d ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autosuggestions ]];then
-        git clone https://github.com/zsh-users/zsh-autosuggestions ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autosuggestions
-    fi &&
-    if [[ ! -d ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-syntax-highlighting ]];then
-        git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-syntax-highlighting
-    fi
-    if [[ ! ( -f $HOME/.zshrc ) ]] || ! diff ~/.zshrc zsh/zshrc > /dev/null;then
-        backup_if_exists ~/.zshrc
-        if uname -a | grep -P '^Linux' > /dev/null;then
-            # Linux
-            ln -s "$(pwd)/zsh/zshrc" ~/.zshrc 
-        else
-            echo '
-            mklink "<1>/.zshrc" "<2>/zsh/zshrc"  
-            ' | sed "s%<1>%$(cygpath -m "$HOME")%" | sed "s%<2>%$(cygpath -m $(pwd))%" >> "$HOME"/mklink.bat
-        fi
-    fi
-    return 0
+function backup_and_replace() {
+  echo # todo
 }
 
-function CONFIG_VIM(){
-    if [[ ! ( -f ~/.vim/autoload/plug.vim ) ]];then
-        echo "can't detect vim-plug, install now"
-        mkdir -p ~/.vim/autoload > /dev/null 2>&1;
-        if [[ $USE_PROXY -ne 0 ]];then
-            # repalce github.com in plug.vim with mirror site or vim-plug can't clone plugins
-            curl -x $PROXY https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim | sed -E 's/github(\\?\.)com/hub.fastgit\1org/g' > ~/.vim/autoload/plug.vim
-        else
-            curl https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim > ~/.vim/autoload/plug.vim
-        fi && echo Install vim-plug successfull! ||
-        (echo "Error when download vim-plug, please check your proxy!" && rm -f ~/.vim/autoload/plug.vim && return 1)
-        
-        # curl -fLo ~/.vim/autoload/plug.vim --create-dirs https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
-    fi
-    if [[ ! ( -f ~/.vimrc ) ]] || ! diff ~/.vimrc vim/vimrc > /dev/null;then
-        backup_if_exists ~/.vimrc
-        if uname -a | grep -q Linux > /dev/null ; then
-            # notice that source file path in symbol link base on 
-            # where the symbol link located if use related path
-            ln -s "$(pwd)/vim/vimrc" ~/.vimrc 
-        else
-            echo '
-            mklink "<1>/.vimrc" "<2>/vim/vimrc"  
-            ' | sed "s%<1>%$(cygpath -m $HOME)%" | sed "s%<2>%$(cygpath -m $(pwd))%" >> $HOME/mklink.bat
-            
-            read -r -p 'Do you want to remap <Esc - CapsLock> ?(y/N)' ans ;ans=${ans^^}
-
-            if echo $ans | grep -P '^Y$' > /dev/null;then
-                start '.\remap.reg'
-                read -r -p 'Remap will make effect after log out and log in, want to log out after install all the dotfiles?(n/Y)' ans1
-                ans1=${ans1^^}
-            fi
-        fi
-    fi
+function install_package() {
+  for pkg in "$@"; do
+    if ! command -v $pkg > /dev/null 2>&1; then TO_INSTALL="$TO_INSTALL $pkg"; fi
+  done
+  if [ -z "$TO_INSTALL" ]; then return 0; fi
+  if LANG= sudo -n -v 2>&1 | grep -q "may not run sudo"; then
+    echo "${FMT_RED}please run script with sudo privilege${FMT_RESET}" && return 255
+  fi
+  $PAC_UPDATE &&
+    $PAC_MAN "$@"
 }
 
-function CONFIG_TMUX(){
-    if [[ ! -f ~/.tmux.conf ]] || ! diff tmux/tmux.conf ~/.tmux.conf;then
-        backup_if_exists ~/.tmux.conf
-        if uname -a | grep -P '^Linux' > /dev/null;then
-            ln -s "$(pwd)/tmux/tmux.conf" ~/.tmux.conf
-        else
-            echo '
-            mklink "<1>/.tmux.conf" "<2>/tmux/tmux.conf"  
-            ' | sed "s%<1>%$(cygpath -m $HOME)%" | sed "s%<2>%$(cygpath -m $(pwd))%" >> $HOME/mklink.bat
-        fi
-    fi
+function git_clone() {
+  install_package git &&
+    git clone "$1"
 }
 
-BACKUP_INIT
-if SET_UP_APP;then
-    CONFIG_ZSH
-fi
-CONFIG_VIM
-CONFIG_TMUX
-git config --global --unset http.https://github.com.proxy
-git config --global --unset https.https://github.com.proxy
-if [[ -f $HOME/mklink.bat ]];then
-    start $HOME/mklink.bat
-fi
-sleep 5
+function install_and_config_zsh() {
+  install_package zsh || {
+    echo -e "${FMT_RED}failed install zsh${FMT_RESET}" && return 255
+  }
+  backup_and_link ~/.zshrc zsh/zshrc &&
+    backup_and_link ~/.oh-my-zsh zsh/oh-my-zsh &&
+    backup_and_link ~/.p10k.zsh zsh/p10k.zsh || { # now use theme p10k, so...
+      echo -e "${FMT_RED}failed config zsh${FMT_RESET}" && return 255
+    }
+  while [ $(basename -- $SHELL) != "zsh" ]; do
+    printf "change default shell to zsh? [y/N]"
+    read -r opt
+    case $opt in
+      y* | Y*) break ;;
+      n* | N* | "") ;;
+    esac
+    sudo -k chsh -s "$(command -v zsh)" "$USER" || {
+      echo "${FMT_RED}${FMT_BOLD}change shell failed!!${FMT_RESET}" && return 255
+    }
+    break
+  done
+  echo -e "${FMT_GREEN}install and config zsh done${FMT_RESET}"
+}
 
-echo "Done"
-rm $HOME/mklink.bat > /dev/null 2>&1
+function install_and_config_vim() {
+  install_package vim || {
+    echo -e "${FMT_RED}failed install vim${FMT_RESET}" && return 255
+  }
+  backup_and_link ~/.vimrc vim/vimrc &&
+    backup_and_link ~/.vim vim/dotvim || {
+      echo -e "${FMT_RED}failed config vim${FMT_RESET}" && return 255
+    }
+  [ -d ~/.vim/autoload ] && mkdir -p ~/.vim/autoload || {
+    echo -e "${FMT_RED}failed make dir $HOME/.vim/autoload" && return 255
+  }
+  [ -e ~/.vim/autoload/plug.vim ] &&
+    ln -s "$(pwd)/vim/vim-plug/plug.vim" ~/.vim/autoload/plug.vim || {
+      echo -e "${FMT_RED}failed create symbol link for $HOME/.vim/autoload/plug.vim"
+      return 255;
+    }
+  echo -e "${FMT_GREEN}install and config vim done${FMT_RESET}"
+}
 
-# echo $ans1
-if [[ -n $ans1 && ${ans1:0:1} != $(echo -e "\n") && ${ans1:0:1} != "N" ]];then
-    cmd <<< logoff
-fi
+function install_and_config_tmux() {
+  install_package tmux || {
+    echo -e "${FMT_RED}failed install tmux${FMT_RESET}" && return 255
+  }
+  backup_and_link ~/.tmux.conf tmux/tmux.conf || {
+    echo -e "${FMT_RED}failed config tmux${FMT_RESET}" && return 255
+  }
+  echo -e "${FMT_GREEN}install and config tmux done${FMT_RESET}"
+}
+
+function show_menu() {
+  echo -e \
+    "input num to choose which to install and config.
+0) all
+1) zsh
+2) vim
+3) tmux
+4) exit"
+  read -p "please input your choose:" num
+  case $num in
+    0)
+      install_and_config_zsh;
+      install_and_config_vim;
+      install_and_config_tmux;
+      exit 0
+      ;;
+    1)
+      install_and_config_zsh;
+      show_menu;
+      ;;
+    2)
+      install_and_config_vim;
+      show_menu;
+      ;;
+    3)
+      install_and_config_tmux;
+      show_menu;
+      ;;
+    4)
+      echo -e "bye" && exit 0;
+      ;;
+  esac
+}
+
+function main() {
+  check_folder_structure;
+  set_up_variable;
+  show_menu;
+}
+
+main;
